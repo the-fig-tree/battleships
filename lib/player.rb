4a6c9c112ecf3bcf5686d724b0a161705356bc21
Simon Woolf <simon@simonwoolf.net>
2013-12-02 10:06:27 +0000

    initial

---
+++ lib/player.rb
@@ -0,0 +1,18 @@
+
+class Player
+  # Initializes a new player with her/his name.
+  #
+  # You will have to create a board in there and randomize the
+  # positioning of your ships on the board.
+  def initialize name
+  end
+  
+  # Tells us if there are still ships that have not been hit
+  # on her/his board.
+  def has_ships_still_floating?
+  end
+  
+  # Shoots at the opponents board.
+  def shoot(at_coordinates, opponent_board)
+  end
+end
